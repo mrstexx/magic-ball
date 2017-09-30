e55179422fa765d3d56a72e60885da38104a6a1a
Stefan Miljevic <stefi5miljevic@gmail.com>
2017-09-30 20:03:42 +0200

    Adding project to GitHub --version 1.0

---
+++ app/src/main/java/com/example/stefa/magicball/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.stefa.magicball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.askButton);
+        final ImageView image = (ImageView) findViewById(R.id.img);
+
+        final int[] images = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5,
+        };
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                image.setImageResource(images[randomGenerator()]);
+                button.setEnabled(false);
+            }
+
+            private int randomGenerator() {
+                Random number = new Random();
+                return number.nextInt(5);
+            }
+        });
+    }
+}
